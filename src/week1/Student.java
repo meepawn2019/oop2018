61318cd2ba5ae40826663bbb64e0ef8ba62f3a88
meepawn <meepawn2018@gmail.com>
2018-09-12 16:32:23 +0700

    Baitap

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,14 +3,33 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
+    private String name;
+    private String id;
+    private String group;
+    private String email;
 
     // TODO: khai báo các phương thức getter, setter cho Student
+    public String getName() {return name;}
+
+    public void setName(String s){name = s;}
+
+    public String getId() { return id;}
+    public void setId(String s) {id = s;}
+    public String getGroup(){ return group;}
+    public void setGroup (String s) {group = s;}
+    public String getEmail(){return email;}
+    public void setEmail(String s) {email = s;}
 
     /**
      * Constructor 1
      */
     Student() {
         // TODO:
+        setName("Student");
+        setId("000");
+        setGroup("INT22041");
+        setEmail("uet@vnu.edu.vn");
+
     }
 
     /**
@@ -21,6 +40,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        setName(n);
+        setId(sid);
+        setEmail(em);
+        setGroup("INT22041");
     }
 
     /**
@@ -29,12 +52,15 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        setName(s.getName());
+        setId(s.getId());
+        setGroup(s.getGroup());
+        setEmail(s.getEmail());
     }
 
     String getInfo() {
         // TODO:
-        //TEST GITHUB
-        //Test 123
+        System.out.println("Ten:" + name + "\n" + "MSSV: " + id + "\n" + "Nhom: "+ group + "\n" + "Email: " + email);
         return null; // xóa dòng này sau khi cài đặt
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,17 +1,21 @@
 package week1;
+import java.util.Scanner;
 
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
+    Student[] students = new Student[100];
 
     public boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        if(s1.getGroup()== s2.getGroup()) return true;
+        else return false;
     }
 
     void studentsByGroup() {
         // TODO:
-    }
+      }
+
 
     void removeStudent(String id) {
         // TODO:
@@ -19,5 +23,32 @@ public class StudentManagement {
 
     public static void main(String[] args) {
         // TODO:
+        Student me = new Student();
+        me.setName("huy");
+        me.setId("17020805");
+        me.setGroup("1");
+        me.setEmail("meepawn2018");
+        String ten;
+        ten = me.getName();
+        System.out.println(ten);
+        me.getInfo();
+        Student t1 = new Student();
+        t1.getInfo();
+        Student t2 = new Student("kael", "123", "cucshit@gmail.com");
+        t2.getInfo();
+        Student t3 = new Student(t2);
+        t3.getInfo();
+        Student t4 = new Student();
+        t4.setGroup("INT22042");
+        StudentManagement a = new StudentManagement();
+        if(a.sameGroup(t4, t1) == true) System.out.println("YES");
+        else System.out.println("NO");
+        if(a.sameGroup(t2, t1) == true) System.out.println("YES");
+        else System.out.println("NO");
+        a.students[0] = t1;
+        a.students[1] = t2;
+        a.students[2] = t3;
+        a.studentsByGroup();
+
     }
 }
